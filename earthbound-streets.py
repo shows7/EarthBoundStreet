49c646a98ecab731a1527e79d46e1869eb223b0e
shows7 <blazekom@gmail.com>
2015-07-13 06:04:22 -0400

    Modified small things

--- earthbound-streets.py
+++ earthbound-streets.py
@@ -14,8 +14,7 @@ COLORS = {'RED':(255,0,0),
          'BLUE':(0,0,255),
          'GREEN':(0,255,0),
          'BLACK':(0,0,0),
-         'WHITE':(255,255,255),
-         'ALTWHITE':(255,255,250)}
+         'WHITE':(255,255,255)}
  
 
 class Player(pygame.sprite.Sprite):    
